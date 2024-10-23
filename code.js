dc51874fca1385c380e5c8334073f62365ade55e
missak-hub <missak225@gmail.com>
2024-10-23 02:42:02 -0700

    Add files via upload

---
+++ code.js
@@ -0,0 +1,12 @@
+const pass='684'
+let no=12345678
+const name1='ak'
+console.table({pass,no,name1});
+console.log(typeof pass);
+console.log(typeof no);
+let num=Number(name1);
+console.log(typeof num);
+console.log(num);
+let name2=String(no);
+console.log(typeof name2);
+console.log(name2);
