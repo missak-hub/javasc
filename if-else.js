2e84af07e2667394459a12ac95e41ed039409ee0
missak-hub <missak225@gmail.com>
2024-10-28 02:44:58 -0700

    Add files via upload

---
+++ if-else.js
@@ -0,0 +1,19 @@
+let singer1=yoyohoneysingh
+let singer2=atiftaslam
+let singer3=farhansaeed
+
+if(singer2==singer3)
+{
+    console.log("greatchoice");
+    
+}
+else if( singer1==singer2)
+{
+    console.log("perfectchoice");
+    
+}
+else
+{
+    console.log("youaremybestsinger");
+    
+}
